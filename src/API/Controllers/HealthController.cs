dccb3236494829c50a3ce3de0b12cf71c66ed7c1
Benjamin Correa <iambenjamin.developer@gmail.com>
2025-07-28 13:59:58 -0300

    health

---
+++ src/API/Controllers/HealthController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
+
+namespace API.Controllers
+{
+    public class HealthController : ControllerBase
+    {
+
+        [HttpGet("/")]
+        [HttpGet("/health")]
+        public IActionResult GetHealth()
+        {
+            var healthInfo = new
+            {
+                status = "UP",
+                utcTime = DateTime.UtcNow,
+                serverTime = DateTime.Now,
+                operatingSystem = RuntimeInformation.OSDescription
+            };
+
+            return Ok(healthInfo);
+        }
+    }
+}
