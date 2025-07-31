c965bbf6aa0694b29720350eb5cedb1b53e4c975
Benjamin Correa <iambenjamin.developer@gmail.com>
2025-07-31 14:59:32 -0300

    Add cache in categories

--- src/Application/Categories/Queries/GetAllCategories/GetAllCategoriesQuery.cs
+++ src/Application/Categories/Queries/GetAllCategories/GetAllCategoriesQuery.cs
@@ -4,6 +4,7 @@ using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Application.Categories.Queries.GetAllCategories
 {
@@ -15,19 +16,32 @@ namespace Application.Categories.Queries.GetAllCategories
     {
         private readonly IApplicationDbContext _context;
         private readonly IMapper _mapper;
+        private readonly IMemoryCache _memoryCache;
+        private const string CacheKey = "categories";
 
-        public GetAllCategoriesQueryHandler(IApplicationDbContext context, IMapper mapper)
+        public GetAllCategoriesQueryHandler(IApplicationDbContext context, IMapper mapper, IMemoryCache memoryCache)
         {
             _context = context;
             _mapper = mapper;
+            _memoryCache = memoryCache;
         }
 
         public async Task<List<CategoryDto>> Handle(GetAllCategoriesQuery request, CancellationToken cancellationToken)
         {
+            // Si ya está en caché, devolverlo directamente
+            if (_memoryCache.TryGetValue(CacheKey, out List<CategoryDto> cachedCategories))
+            {
+                return cachedCategories;
+            }
+
             var dtos = await _context.Categories
                             .ProjectTo<CategoryDto>(_mapper.ConfigurationProvider)
                             .OrderBy(x => x.Name)
                             .ToListAsync(cancellationToken);
+
+            // Guardar en caché por 30 minutos
+            _memoryCache.Set(CacheKey, dtos, TimeSpan.FromMinutes(30));
+
             return dtos;
         }
     }
