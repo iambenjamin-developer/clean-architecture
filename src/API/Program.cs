2364cf67771b521ee7ad12b8b6506e0c36cac13a
Benjamin Correa <iambenjamin.developer@gmail.com>
2025-07-31 13:04:42 -0300

    Add seeder

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Infrastructure.Persistence;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -11,9 +12,35 @@ namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            // Migrate and Seed the DB
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+
+                    // Optional: apply migrations
+                    await context.Database.EnsureCreatedAsync();
+                    // await context.Database.MigrateAsync(); //  si usas Migrations en lugar de EnsureCreated
+
+                    // Call your seeder
+                    await ApplicationDbContextSeed.SeedSampleDataAsync(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                    throw;
+                }
+            }
+
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
