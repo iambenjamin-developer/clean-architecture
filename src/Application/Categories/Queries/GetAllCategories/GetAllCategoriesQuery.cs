06b748872ac22ff153099902e892bc10c39456e6
Benjamin Correa <iambenjamin.developer@gmail.com>
2025-07-31 14:53:27 -0300

    GetAllCategories

---
+++ src/Application/Categories/Queries/GetAllCategories/GetAllCategoriesQuery.cs
@@ -0,0 +1,34 @@
+﻿using Application.Categories.DTOs;
+using Application.Common.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Categories.Queries.GetAllCategories
+{
+    public class GetAllCategoriesQuery : IRequest<List<CategoryDto>>
+    {
+    }
+
+    public class GetAllCategoriesQueryHandler : IRequestHandler<GetAllCategoriesQuery, List<CategoryDto>>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetAllCategoriesQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<List<CategoryDto>> Handle(GetAllCategoriesQuery request, CancellationToken cancellationToken)
+        {
+            var dtos = await _context.Categories
+                            .ProjectTo<CategoryDto>(_mapper.ConfigurationProvider)
+                            .OrderBy(x => x.Name)
+                            .ToListAsync(cancellationToken);
+            return dtos;
+        }
+    }
+}
